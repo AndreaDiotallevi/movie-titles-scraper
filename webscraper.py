e7c15edcf4e287f99728261340602c80442ccb47
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-02-23 11:01:44 +0000

    Save data into json file

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = "http://istanbulfilms.blogspot.com/2010/05/top-films-from-guinea-1.html"
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
+
+movieTitles = {}
 
 for li in content.findAll("li"):
   response = requests.get(li.find("a").get("href"), timeout=5)
@@ -11,9 +14,13 @@ for li in content.findAll("li"):
   div = content.find(name="div", attrs={"id":"main"})
   for i, b in enumerate(content.findAll("b")):
     if i == 0:
-      country = b.text.split("FROM ")[-1].capitalize().encode("utf-8")
+      country = b.text.split("FROM ")[-1].capitalize()
+      if len(country.replace(" ", "")) == 0:
+        break
       print(country)
       movies = []
     elif (not b.find("a")) and (b.text.strip()):
       movies.append(b.text)
-  print (movies)
+  movieTitles[country] = movies
+with open("movieTitles.json", "w") as outfile:
+  json.dump(movieTitles, outfile, indent=2)
