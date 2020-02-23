f003b0b91a4eccc5fb5c836c1831bf6dad6dafd6
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-02-23 10:43:48 +0000

    Add code to get country name and the list of movies

--- webscraper.py
+++ webscraper.py
@@ -5,4 +5,15 @@ url = "http://istanbulfilms.blogspot.com/2010/05/top-films-from-guinea-1.html"
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-print (content)
+for li in content.findAll("li"):
+  response = requests.get(li.find("a").get("href"), timeout=5)
+  content = BeautifulSoup(response.content, "html.parser")
+  div = content.find(name="div", attrs={"id":"main"})
+  for i, b in enumerate(content.findAll("b")):
+    if i == 0:
+      country = b.text.split("FROM ")[-1].capitalize().encode("utf-8")
+      print(country)
+      movies = []
+    elif (not b.find("a")) and (b.text.strip()):
+      movies.append(b.text)
+  print (movies)
