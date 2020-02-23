6851ad3140d5da33e3da97cab36a2d8aeb0ec81b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-02-23 12:20:41 +0000

    Add unidecode library to decode non-ascii characters

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from unidecode import unidecode
 
 url = "http://istanbulfilms.blogspot.com/2010/05/top-films-from-guinea-1.html"
 response = requests.get(url, timeout=5)
@@ -14,13 +15,12 @@ for li in content.findAll("li"):
   div = content.find(name="div", attrs={"id":"main"})
   for i, b in enumerate(content.findAll("b")):
     if i == 0:
-      country = b.text.split("FROM ")[-1].capitalize()
-      if len(country.replace(" ", "")) == 0:
-        break
-      print(country)
+      country = unidecode(b.text.split("FROM ")[-1]).capitalize().strip()
       movies = []
     elif (not b.find("a")) and (b.text.strip()):
-      movies.append(b.text)
-  movieTitles[country] = movies
+      movies.append(unidecode(b.text).strip())
+  if country != "":
+    print(country)
+    movieTitles[country] = movies
 with open("movieTitles.json", "w") as outfile:
   json.dump(movieTitles, outfile, indent=2)
